687f001cf4665bc88f27fda7c789a1ce540e68b0
Chao Chen <chao.chen@gatech.edu>
2025-08-03 12:15:03 -0500

    update generator

--- Gauss_Seidel_C++.cpp
+++ Gauss_Seidel_C++.cpp
@@ -1,153 +1,155 @@
-//C++ Program to Implement Gauss Seidel Method
+// C++ Program to Implement Gauss Seidel Method
 
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <unistd.h>
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-#define EPSILON 0.001  // till 3 correct decimal places
+#define EPSILON 0.000000001 // till 3 correct decimal places
 
-
-//Helper function
-float findSum(int i, std::vector<vector<float>> a,int n)
-{
-     float sum=0;
-     for(int j=0;j<n;j++)
-     {
-       if(i!=j)
-         sum+=a[i][j];
-     }
-     return sum;
+// Helper function
+float findSum(int i, std::vector<vector<float>> a, int n) {
+  float sum = 0;
+  for (int j = 0; j < n; j++) {
+    if (i != j)
+      sum += a[i][j];
+  }
+  return sum;
 }
 
-//checks if Gauss Seidel Method is applicable and return true if yes otherwise return false
-bool isMethodApplicable(vector<vector<float>> a,int n)
-{
-   for(int i=0;i<n;i++) {
-       for(int j=0;j<n;j++) {
-           if(fabs(a[i][i])>findSum(i,a,n))
-              break;
-           else
-              return false;
-       }
-   }
-   return true;
+// checks if Gauss Seidel Method is applicable and return true if yes otherwise
+// return false
+bool isMethodApplicable(vector<vector<float>> a, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (fabs(a[i][i]) > findSum(i, a, n))
+        break;
+      else
+        return false;
+    }
+  }
+  return true;
 }
 
-//prints the Value of Unknowns
-void print(int iteration, float values[],int n)
-{
-  cout<<"Iteration "<<iteration<< ": ";
-//   for(int i=0;i<n;i++)
-//     cout<<"value["<<i+1<<"]= "<<values[i]<<" ";
-  cout<<endl;
+// prints the Value of Unknowns
+void print(int iteration, float values[], int n) {
+#if 0
+  cout << "Iteration " << iteration << ": ";
+  for (int i = 0; i < n; i++)
+    cout << "value[" << i + 1 << "]= " << values[i] << " ";
+  cout << endl;
+#endif
 }
 
+void findValues(vector<vector<float>> a, int maxIterations, float values_old[],
+                int n) {
+  int i, j, k, iteration, flag = 0;
+  float ratio, sum = 0;
+  float values_new[n];
+  // This loop is added for Gauss seidel ( not present in Jacobi method)
+  for (int i = 0; i < n; i++)
+    values_new[i] = 0;
 
-void findValues(vector<vector<float>> a, int maxIterations, float values_old[],int n)
-{
-    int i,j,k,iteration,flag=0;
-    float ratio,sum=0;
-    float values_new[n];
-    //This loop is added for Gauss seidel ( not present in Jacobi method)
-    for(int i=0;i<n;i++)
-     values_new[i]=0;
+  for (iteration = 1; iteration <= maxIterations; iteration++) {
+    for (i = 0; i < n; i++) {
+      sum = 0;
+      for (j = 0; j < n; j++) {
+        if (i != j)
+          sum += a[i][j] *
+                 values_new[j]; // in Gauss Jacobi, we took values_old[] here
+      }
 
-    for(iteration=1;iteration<=maxIterations;iteration++)
-    {
-        for(i=0;i<n;i++)
-        {
-            sum=0;
-            for(j=0;j<n;j++)
-            {
-                if(i!=j)
-                sum+=a[i][j]*values_new[j];   // in Gauss Jacobi, we took values_old[] here
-            }
+      values_new[i] = (a[i][n] - sum) / a[i][i];
+    }
+    // Now you have found the values of n unknowns for above iteration
+    // Now check if your matching criteria satisfied , comparing with previous
+    // iteration values
+    for (k = 0; k < n; k++) {
+      if (fabs(values_old[k] - values_new[k]) < EPSILON) {
+        continue;
+      } else {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0) {
+      // print final values of unknowns and return
+      print(iteration, values_new, n);
+      // cout << "Return with accurate answer with niters = " << iteration <<
+      // "\n"; return;
+      break;
+    }
+    flag = 0; // resetting the flag
 
-            values_new[i]=(a[i][n] - sum)/a[i][i];
-        }
-        //Now you have found the values of n unknowns for above iteration
-        //Now check if your matching criteria satisfied , comparing with previous iteration values
-        for(k=0;k<n;k++)
-        {
-            if(fabs(values_old[k]-values_new[k])<EPSILON)
-                continue;
-            else {
-               flag=1;
-               break;
-            }
-        }
-        if(flag==0) {
-            print(iteration,values_new,n);  // print final values of unknowns  and return
-            cout << "Return with accurate answer with niters = " << iteration << "\n";
-            return ;
-        }
-        flag=0; //resetting the flag
+    print(iteration, values_new, n); // To print intermediate roots
 
-        print(iteration,values_new,n); //To print intermediate roots
+    // copy new values of unknowns to old value array
+    for (k = 0; k < n; k++)
+      values_old[k] = values_new[k];
+  } // end of iteration loop
+  print(iteration, values_new, n);
+  cout << "Tot number of iterations: " << iteration << endl;
 
-        //copy new values of unknowns to old value array
-        for(k=0;k<n;k++)
-             values_old[k]=values_new[k];
-    } //end of iteration loop
-    print(iteration,values_new,n) ;
-    cout << "Tot number of iterations: " << iteration << endl;
-
-} //end of findValues()
-
+} // end of findValues()
 
 void generateMatrix(vector<vector<float>> &a, int n) {
-    srand(time(0)); // Seed for random number generation
-    for (int i = 0; i < n; i++) {
-        float rowSum = 0; // Sum of off-diagonal elements in the row
-
-        for (int j = 0; j < n; j++) {
-            if (i != j) {
-                // Generate random off-diagonal elements in the range [-10, 10]
-                a[i][j] = static_cast<float>(rand() % 21 - 10);
-                rowSum += fabs(a[i][j]);
-            }
-        }
-
-        // Set diagonal element to ensure diagonal dominance
-        a[i][i] = rowSum + static_cast<float>(rand() % 10 + 1); // Slightly larger than rowSum
-
-        // Generate random constant for the last column
-        a[i][n] = static_cast<float>(rand() % 100 + 1); // Random constant in the range [1, 100]
+  srand(
+      static_cast<unsigned int>(time(0)) ^
+      static_cast<unsigned int>(getpid())); // Seed for random number generation
+  for (int i = 0; i < n; i++) {
+    float rowSum = 0;
+    for (int j = 0; j < n; j++) {
+      if (i != j) {
+        // Random float in [-10, 10]
+        a[i][j] = static_cast<float>(rand()) / RAND_MAX * 2.0f - 1.0f;
+        rowSum += fabs(a[i][j]);
+      }
     }
+    // Diagonal element: strictly greater than sum of off-diagonal elements
+    a[i][i] = rowSum + static_cast<float>(rand()) / RAND_MAX * 5.0f + 1.0f;
+    // Random constant for augmented column
+    a[i][n] = static_cast<float>(rand()) / RAND_MAX * 10.0f + 1.0f;
+  }
 }
 
+int main(int argc, char *argv[]) {
+  int i, j, k, x, y, maxIterations, n;
+  float ratio;
+  if (argc < 2) {
+    cout << "Usage: " << argv[0] << " <n> [maxIterations]\n";
+    return 1;
+  }
+  n = atoi(argv[1]);
+  if (argc >= 3)
+    maxIterations = atoi(argv[2]);
+  else
+    maxIterations = 10000;
+  vector<vector<float>> a(500, vector<float>(501));
+  float values[500];
 
-int main()
-{
-    int i,j,k,x,y,maxIterations,n;
-    float ratio;
-    cout<<"Enter no of Unknowns\n";
-    cin>>n;
-    cout<<"Enter no. of iterations\n";
-    cin>>maxIterations;
-    vector<vector<float>> a(500, vector<float>(501));
-    float values[500];
-
-    generateMatrix(a, n);
-    cout<<"Generated the Augmented Matrix\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= n; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
+  generateMatrix(a, n);
+  cout << "\nThe Augmented Matrix (" << n << "x" << n << ")\n";
+  // Print the augmented matrix with aligned columns
+  cout << fixed;
+  cout.precision(5);
+  int width = 12;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << setw(width) << a[i][j];
     }
+    cout << endl;
+  }
 
-   if(!isMethodApplicable(a,n))
-   {
-        cout<<"\nGauss Seidel Method can't be applied";
-        return 0;
-   }
-   cout<<"\nGauss Seidel Method is applicable\n\n";
-   for(int i=0;i<n;i++)
-     values[i]=0;
-   findValues(a,maxIterations,values,n);
-   return 0;
+  if (!isMethodApplicable(a, n)) {
+    cout << "\nGauss Seidel Method can't be applied";
+    return 0;
+  }
+  // cout << "\nGauss Seidel Method is applicable\n\n";
+  for (int i = 0; i < n; i++)
+    values[i] = 0;
+  findValues(a, maxIterations, values, n);
+  return 0;
 }
-
-
-
